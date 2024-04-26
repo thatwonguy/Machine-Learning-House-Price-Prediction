3e2a54f5f0960a6564f6020247b703c8a8cf0c07
thatwonguy <monsore1337@gmail.com>
2024-04-25 23:31:49 -0700

    description added to app

--- app.py
+++ app.py
@@ -13,6 +13,17 @@ st.write("""
 # California House Price Prediction App
 
 This app predicts **California House Prices** based on your chosen inputs using Machine Learning.
+
+**DESCRIPTION:**
+
+The US Census Bureau has published California Census Data which has 10 types of metrics such as the population, median income, median housing price, and so on for each block group in California. The dataset also serves as an input for project scoping and tries to specify the functional and nonfunctional r requirements for it.
+
+Problem Objective: The project aims at building a model of housing prices to predict median house values in California using the provided dataset. This model should learn from the data and be able to predict the median housing price in any district, given all the other metrics.
+
+Districts or block groups are the smallest geographical units for which the US Census Bureau publishes sample data (a block group typically has a population of 600 to 3,000 people). There are 20,640 districts in the project dataset.
+
+Domain: Finance and Housing         
+
 """)
 st.write('---')
 
