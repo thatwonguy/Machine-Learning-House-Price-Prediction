d58fb8f84fd73583c4238eb561ba2d8e0b11e25a
thatwonguy <78534460+thatwonguy@users.noreply.github.com>
2024-05-27 15:21:45 -0700

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ This app predicts **California House Prices** based on your chosen inputs using 
 
 **DESCRIPTION:**
 
-The US Census Bureau has published California Census Data which has 10 types of metrics such as the population, median income, median housing price, and so on for each block group in California. The dataset also serves as an input for project scoping and tries to specify the functional and nonfunctional r requirements for it.
+The US Census Bureau has published California Census Data which has 10 types of metrics such as the population, median income, median housing price, and so on for each block group in California. The dataset also serves as an input for project scoping and tries to specify the functional and nonfunctional requirements for it.
 
 Districts or block groups are the smallest geographical units for which the US Census Bureau publishes sample data (a block group typically has a population of 600 to 3,000 people). There are 20,640 districts in the project dataset.   
 
